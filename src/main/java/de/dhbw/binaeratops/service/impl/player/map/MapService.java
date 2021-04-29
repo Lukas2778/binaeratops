ff2cedc404edf416767200cf4b3400bfbe5c101f
Lukas2778 <i19015@hb.dhbw-stuttgart.de>
2021-04-29 14:25:29 +0200

    FEATURE: Wände zur Map hinzugefügt.

--- src/main/java/de/dhbw/binaeratops/service/impl/player/map/MapService.java
+++ src/main/java/de/dhbw/binaeratops/service/impl/player/map/MapService.java
@@ -1,4 +1,155 @@
 package de.dhbw.binaeratops.service.impl.player.map;
 
+import java.util.Arrays;
+
 public class MapService {
+    //kann Raum an bestimmter Stelle gesetzt werden
+    //kann Raum an bestimmter Stelle gelöscht werden
+    //kann Mauer an bestimmter Stelle gesetzt werden
+    //kann Mauer an bestimmter Stelle gelöscht werden (nur wenn Bild)
+    //Objekt, das den aktuellen Dungeon speichert
+    //Größe im Vorfeld festlegen 10x10 oder 8x8
+
+    private int MAP_SIZE;
+    Boolean[][] roomsSet; //Array mit Räumen
+    Boolean[][] wallSet; //Array mit Mauern
+    Boolean[][] falseTmp;
+
+    public MapService(int AMapSize) {
+        MAP_SIZE = AMapSize;
+        roomsSet = new Boolean[MAP_SIZE][MAP_SIZE];
+        wallSet = new Boolean[MAP_SIZE][MAP_SIZE];
+        falseTmp = new Boolean[MAP_SIZE][MAP_SIZE];
+        //Karte mit keinem Raum
+        for (int i = 0; i < MAP_SIZE; ++i) {
+            Arrays.fill(roomsSet[i], Boolean.FALSE);
+            Arrays.fill(wallSet[i], Boolean.FALSE);
+            Arrays.fill(falseTmp[i], Boolean.FALSE);
+        }
+    }
+
+    public void setMAP_SIZE(final int AMapSize) {
+        MAP_SIZE = AMapSize;
+    }
+
+    public int getMAP_SIZE() {
+        return MAP_SIZE;
+    }
+
+    public boolean roomExists(int ALocationX, int ALocationY) {
+        return roomsSet[ALocationX][ALocationY];
+        //wenn Raum schon existiert, muss in der View mittels der entsprechenden ID die Raumeinstellungen angeigt werden
+    }
+
+    public boolean canPlaceRoom(int ALocationX, int ALocationY) {
+        //Überprüfen, ob an der Position schon ein Raum existiert
+        if (roomsSet[ALocationX][ALocationY]) {
+            return false;
+        }
+
+        //Überprüfen, ob schon ein Feld gesetzt wurde
+        if (Arrays.deepEquals(falseTmp, roomsSet)) {
+            return true;
+        } else {
+            //überprüfen, ob geklicktes Feld der Nachbar ist
+            //Nachbar Norden
+            if (!(ALocationX - 1 < 0)) {
+                if (roomsSet[ALocationX - 1][ALocationY]) {
+                    return true;
+                }
+            }
+            //Nachbar Osten
+            if (!(ALocationY + 1 > MAP_SIZE - 1)) {
+                if (roomsSet[ALocationX][ALocationY + 1]) {
+                    return true;
+                }
+            }
+            //Nachbar Süden
+            if (!(ALocationX + 1 > MAP_SIZE - 1)) {
+                if (roomsSet[ALocationX + 1][ALocationY]) {
+                    return true;
+                }
+            }
+            //Nachbar Westen
+            if (!(ALocationY - 1 < 0)) {
+                if (roomsSet[ALocationX][ALocationY - 1])
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public void placeRoom(int ALocationX, int ALocationY) {
+        //true in roomSet an bestimmte Stelle speichern
+        //Raum erzeugen und in Datenbank speichern -> Raum_ID
+        //Raum_ID wird an Dungeon
+        roomsSet[ALocationX][ALocationY] = true;
+    }
+
+    public boolean canDeleteRoom(int ALocationX, int ALocationY) {
+        //überprüfen, ob weniger als zwei Nachbarn
+        int neighborCount = 0;
+//        boolean neighborNorth = false;
+//        boolean neighborEast = false;
+//        boolean neighborSouth = false;
+//        boolean neighborWest = false;
+        //Nachbar Norden
+        if (!(ALocationX - 1 < 0)) {
+            if (roomsSet[ALocationX - 1][ALocationY]) {
+                neighborCount++;
+//                neighborNorth = true;
+            }
+        }
+        //Nachbar Osten
+        if (!(ALocationY + 1 > MAP_SIZE - 1)) {
+            if (roomsSet[ALocationX][ALocationY + 1]) {
+                neighborCount++;
+//                neighborEast = true;
+            }
+        }
+        //Nachbar Süden
+        if (!(ALocationX + 1 > MAP_SIZE - 1)) {
+            if (roomsSet[ALocationX + 1][ALocationY]) {
+                neighborCount++;
+//                neighborSouth = true;
+            }
+        }
+        //Nachbar Westen
+        if (!(ALocationY - 1 < 0)) {
+            if (roomsSet[ALocationX][ALocationY - 1]) {
+                neighborCount++;
+//                neighborWest = true;
+            }
+        }
+        //Ergebnis prüfen
+        if (neighborCount < 2) {
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteRoom(int ALocationX, int ALocationY) {
+        roomsSet[ALocationX][ALocationY] = false;
+    }
+
+    public boolean wallExists(int ALocationX, int ALocationY) {
+        return wallSet[ALocationX][ALocationY];
+    }
+
+    public boolean canPlaceWall(int ALocationX, int ALocationY) {
+        return true;
+    }
+
+    public void placeWall(int ALocationX, int ALocationY) {
+        wallSet[ALocationX][ALocationY] = true;
+    }
+
+    public boolean canDeleteWall(int ALocationX, int ALocationY) {
+        return true;
+    }
+
+    public void deleteWall(int ALocationX, int ALocationY) {
+        wallSet[ALocationX][ALocationY] = false;
+    }
+
 }
--- src/main/java/de/dhbw/binaeratops/view/player/Map/MapView.java
+++ src/main/java/de/dhbw/binaeratops/view/player/Map/MapView.java
@@ -6,39 +6,29 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import de.dhbw.binaeratops.service.impl.player.map.MapService;
 
-import java.util.Arrays;
+
 @CssImport("./views/player/map/map.css")
 
 @RouteAlias("")
 @Route("login")
 public class MapView extends VerticalLayout {
 
+    MapService myMapService;
 
     public MapView() {
+        addClassName("map-view");
+        int width = 10;
+        myMapService = new MapService(width);
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setAlignItems(Alignment.CENTER);
-
         VerticalLayout lines = new VerticalLayout();
         lines.setSpacing(false);
 
         HorizontalLayout lineRoomBorder;
         HorizontalLayout line;
-
-        int width=10;
-        //int counter = 0;
-        Boolean clicked[][] = new Boolean[width][width];
-        //alle Werte auf false setzen
-        for (int i = 0; i < width; ++i) {
-            Arrays.fill(clicked[i], Boolean.FALSE);
-        }
-        Boolean tmp[][] = new Boolean[width][width];
-        //alle Werte auf false setzen
-        for (int i = 0; i < width; ++i) {
-            Arrays.fill(tmp[i], Boolean.FALSE);
-        }
-
 
         for (int i = 0; i < width; ++i) {
             line = new HorizontalLayout();
@@ -53,123 +43,71 @@ public class MapView extends VerticalLayout {
                 myButt.addClassName("button-container");
                 myButt.addClassName("buttRoom");
 
-                NativeButton borderButtHorizon = new NativeButton();
-                borderButtHorizon.addClassName("button-container");
-                borderButtHorizon.addClassName("buttHorizontal");
+                NativeButton borderHorizonButt = new NativeButton();
+                borderHorizonButt.addClassName("button-container");
+                borderHorizonButt.addClassName("buttHorizontal");
 
-                NativeButton borderButtVert = new NativeButton();
-                borderButtVert.addClassName("button-container");
-                borderButtVert.addClassName("buttVertical");
+                NativeButton borderVertButt = new NativeButton();
+                borderVertButt.addClassName("button-container");
+                borderVertButt.addClassName("buttVertical");
 
                 int finalI = i;
                 int finalJ = j;
                 myButt.addClickListener(
                         e -> {
-                            //Feld anwählen
-                            if (!clicked[finalI][finalJ]) {
-                                //Überprüfen, ob schon ein Feld gesetzt wurde
-                                if (Arrays.deepEquals(tmp, clicked)) {
-                                    myButt.getStyle().set("background", "red");
-                                    clicked[finalI][finalJ] = true;
-                                } else {
-                                    //überprüfen, ob geklicktes Feld der Nachbar ist
-
-                                    //Nachbar Norden
-                                    if (!(finalI - 1 < 0)) {
-                                        if (clicked[finalI - 1][finalJ]) {
-                                            myButt.getStyle().set("background", "red");
-                                            clicked[finalI][finalJ] = true;
-                                        }
-                                    }
-
-                                    //Nachbar Osten
-                                    if (!(finalJ + 1 > width - 1)) {
-                                        if (clicked[finalI][finalJ + 1]) {
-                                            myButt.getStyle().set("background", "red");
-                                            clicked[finalI][finalJ] = true;
-                                        }
-                                    }
-
-                                    //Nachbar Süden
-                                    if (!(finalI + 1 > width - 1)) {
-                                        if (clicked[finalI + 1][finalJ]) {
-                                            myButt.getStyle().set("background", "red");
-                                            clicked[finalI][finalJ] = true;
-                                        }
-                                    }
-
-                                    //Nachbar Westen
-                                    if (!(finalJ - 1 < 0)) {
-                                        if (clicked[finalI][finalJ - 1]) {
-                                            myButt.getStyle().set("background", "red");
-                                            clicked[finalI][finalJ] = true;
-                                        }
-                                    }
+                            if (!myMapService.roomExists(finalI, finalJ)) {
+                                //Feld anwählen
+                                if (myMapService.canPlaceRoom(finalI, finalJ)) {
+                                    myMapService.placeRoom(finalI, finalJ);
+                                    myButt.getStyle().set("background", "blue");
                                 }
                             }
                             //Feld abwählen
                             else {
-                                //wenn mehr als einen Nachbar, ist Feld nicht löschbar, außer es gibt vier Felder bei denen eines davon gelöscht werden soll, aber keine Felder sonst anschließen
-                                boolean deleatable = false;
-
-                                //überprüfen, ob weniger als zwei Nachbarn
-                                int neighborCount = 0;
-                                boolean neighborNorth = false;
-                                boolean neighborEast = false;
-                                boolean neighborSouth = false;
-                                boolean neighborWest = false;
-                                //Nachbar Norden
-                                if (!(finalI - 1 < 0)) {
-                                    if (clicked[finalI - 1][finalJ]) {
-                                        neighborCount++;
-                                        neighborNorth = true;
-                                    }
-                                }
-                                //Nachbar Osten
-                                if (!(finalJ + 1 > width - 1)) {
-                                    if (clicked[finalI][finalJ + 1]) {
-                                        neighborCount++;
-                                        neighborEast = true;
-                                    }
-                                }
-                                //Nachbar Süden
-                                if (!(finalI + 1 > width - 1)) {
-                                    if (clicked[finalI + 1][finalJ]) {
-                                        neighborCount++;
-                                        neighborSouth = true;
-                                    }
-                                }
-                                //Nachbar Westen
-                                if (!(finalJ - 1 < 0)) {
-                                    if (clicked[finalI][finalJ - 1]) {
-                                        neighborCount++;
-                                        neighborWest = true;
-                                    }
-                                }
-                                //Ergebnis prüfen
-                                if (neighborCount < 2) {
-                                    deleatable = true;
-                                }
-//                                else{
-//                                    //es gibt vier Felder bei denen eines davon gelöscht werden soll, aber keine Felder sonst anschließen
-//                                    if((neighborNorth&&neighborWest)||(neighborNorth&&neighborEast)||(neighborSouth&&neighborWest)||(neighborSouth&&neighborEast)){
-//
-//                                    }
-//                                }
-                                if (deleatable) {
+                                if (myMapService.canDeleteRoom(finalI, finalJ)) {
+                                    myMapService.deleteRoom(finalI, finalJ);
                                     myButt.getStyle().set("background", "grey");
-                                    clicked[finalI][finalJ] = false;
                                 }
                             }
                         });
+                borderHorizonButt.addClickListener(e->{
+                    if(!myMapService.wallExists(finalI,finalJ)){
+                        //Mauer anwählen
+                        if(myMapService.canPlaceWall(finalI,finalJ)){
+                            myMapService.placeWall(finalI,finalJ);
+                            borderHorizonButt.getStyle().set("background", "brown");
+                        }
+                    }else{
+                        //Mauer abwählen
+                        if(myMapService.canDeleteWall(finalI,finalJ)){
+                            myMapService.deleteWall(finalI,finalJ);
+                            borderHorizonButt.getStyle().set("background", "grey");
+                        }
+                    }
+                });
+                borderVertButt.addClickListener(e->{
+                    if(!myMapService.wallExists(finalI,finalJ)){
+                        //Mauer anwählen
+                        if(myMapService.canPlaceWall(finalI,finalJ)){
+                            myMapService.placeWall(finalI,finalJ);
+                            borderVertButt.getStyle().set("background", "brown");
+                        }
+                    }else{
+                        //Mauer abwählen
+                        if(myMapService.canDeleteWall(finalI,finalJ)){
+                            myMapService.deleteWall(finalI,finalJ);
+                            borderVertButt.getStyle().set("background", "grey");
+                            //Layout neu erstellen, da sonst leere Bereiche der Mauern oder Mauern extra Bereich
+                        }
+                    }
+                });
                 line.add(myButt);
                 if (finalJ < width - 1) {
-                    line.add(borderButtVert);
+                    line.add(borderVertButt);
                 }
                 if (finalI < width - 1) {
-                    lineRoomBorder.add(borderButtHorizon);
+                    lineRoomBorder.add(borderHorizonButt);
                 }
-                //++counter;
             }
             lines.add(line);
             lines.add(lineRoomBorder);
