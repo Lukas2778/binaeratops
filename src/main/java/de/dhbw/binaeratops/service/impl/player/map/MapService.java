0c2d53a89230f46909835b5895669b1ae817433c
Lukas2778 <i19015@hb.dhbw-stuttgart.de>
2021-04-29 11:26:01 +0200

    FEATURE: Map view hinzugefügt und Route der Login view überschrieben.

---
+++ src/main/java/de/dhbw/binaeratops/service/impl/player/map/MapService.java
@@ -0,0 +1,4 @@
+package de.dhbw.binaeratops.service.impl.player.map;
+
+public class MapService {
+}
---
+++ src/test/java/de/dhbw/binaeratops/model/service/impl/player/map/MapServiceTest.java
@@ -0,0 +1,11 @@
+package de.dhbw.binaeratops.model.service.impl.player.map;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class MapServiceTest {
+
+}
