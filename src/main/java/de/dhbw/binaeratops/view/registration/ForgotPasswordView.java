9ef1e124b319fc9688ef040b722ec3cb79b47534
Lukas2778 <i19015@hb.dhbw-stuttgart.de>
2021-04-29 20:25:34 +0200

    DOCS: Überschriften und Texte überarbeitet.

--- src/main/java/de/dhbw/binaeratops/view/registration/ForgotPasswordView.java
+++ src/main/java/de/dhbw/binaeratops/view/registration/ForgotPasswordView.java
@@ -9,6 +9,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.VaadinSession;
 import de.dhbw.binaeratops.service.api.registration.AuthServiceI;
 import de.dhbw.binaeratops.service.impl.registration.FalseUserException;
